f4750a2ee244d6e12d9078d4bdd13b9301eedee1
Damiansebastians <116028887+Damiansebastians@users.noreply.github.com>
2023-06-25 18:17:30 +0200

    working counter

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+// SLIDER ABOUT US
+
+var swiper_about = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
